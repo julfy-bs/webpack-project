03405aa5e399b0754b57b74cc70e38517819116c
Bogdan Sutuzhko <bogdan.sutuzhko.19@gmail.com>
2020-11-05 11:46:40 +0300

    s

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,36 @@
+const PostcssEasyImport = require("postcss-easy-import");
+const Autoprefixer = require("autoprefixer");
+const PostcssAdvancedVariables = require("postcss-advanced-variables");
+const PostcssNested = require("postcss-nested");
+const PostcssRgb = require("postcss-rgb");
+const PostcssInlineSvg = require("postcss-inline-svg");
+const CssNano = require("cssnano");
+const PostcssPxToRem = require("postcss-pxtorem");
 const fs = require("fs");
 
 module.exports = {
   syntax: "postcss-scss",
   parser: "postcss-scss",
   plugins: [
-    require("postcss-easy-import")({
+    PostcssEasyImport({
       extensions: ".pcss",
     }),
-    require("autoprefixer")({
+    Autoprefixer({
       cascade: false,
     }),
-    require("postcss-advanced-variables")({
+    PostcssAdvancedVariables({
       variables: JSON.parse(
         fs.readFileSync("./src/styles/variables.json", "utf-8")
       ),
     }),
-    require("postcss-nested"),
-    require("postcss-rgb"),
-    require("postcss-inline-svg")({
-      removeFill: true,
+    PostcssNested,
+    PostcssRgb,
+    PostcssInlineSvg({
+      removeFill: false,
       path: "./src/images/icons",
     }),
-    require("cssnano"),
-    require("postcss-pxtorem")({
+    CssNano,
+    PostcssPxToRem({
       rootValue: 16,
       propList: ["*", "!*border*"],
       selectorBlackList: [/^html$/],
